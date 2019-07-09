c1d3b1ee74505cba06fcc1970a48d47de1cf241e
Baptiste Esteban <baptiste.esteban@epita.fr>
2019-07-09 11:36:56 +0000

    [FILESYSTEM]: add code

--- src/file-system/fs.hh
+++ src/file-system/fs.hh
@@ -1,5 +1,13 @@
 #pragma once
 
+enum command_code
+{
+  READ_CODE,
+  WRITE_CODE,
+  CREATE_CODE,
+  DELETE_CODE
+};
+
 int create(const std::string& name);
 
 std::string read(const std::string& name);
