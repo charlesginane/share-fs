236e5b92eba5daaf28f0523a507af254e5b064ab
Baptiste Esteban <baptiste.esteban@epita.fr>
2019-07-09 10:31:10 +0000

    [PROCESS]: add constructors

--- src/file-system/process.hh
+++ src/file-system/process.hh
@@ -9,22 +9,41 @@ enum
 
 struct Process
 {
-  std::size_t id;
-  std::size_t size;
+  Process(std::size_t cid, std::size_t csize)
+    : id(cid)
+    , size(csize)
+    {}
+  
+  const std::size_t id;
+  const std::size_t size;
 };
 
 struct Leader : public Process
 {
+  Leader(std::size_t cid, std::size_t csize)
+    : Process(cid, csize)
+    {}
+  
   std::map<std::string, std::size_t> location;
 };
 
 struct Client : public Process
 {
+  Client(std::size_t cid, std::size_t csize, std::size_t cchief)
+    : Process(cid, csize)
+    , chief(cchief)
+    {}
+  
   std::size_t chief;
 };
 
 struct File : public Process
 {
+  File(std::size_t cid, std::size_t csize)
+    : Process(cid, csize)
+    , hasNext(false)
+    {}
+
   char content[SIZE_FILE];
   bool hasNext;
   std::size_t next;
